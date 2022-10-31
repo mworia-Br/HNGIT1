20385ecd50a7a9c1dc3457fc1fb07a13dc78dba7
Brian Nganga <briannganga70@gmail.com>
2022-10-31 22:07:24 +0300

    done

--- apitask/models.py
+++ apitask/models.py
@@ -5,7 +5,7 @@ class Task1(models.Model):
     slackUsername=models.CharField(max_length=25)
     backend=models.BooleanField(default=True)
     age=models.IntegerField(default=0)
-    bio=models.CharField(max_length=25)
+    bio=models.CharField(max_length=300)
 
     def __str__(self):
         return self.slackUsername
