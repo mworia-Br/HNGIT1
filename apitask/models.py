795a4c3061e97b3147726b0623c7a210525f026a
Brian Nganga <briannganga70@gmail.com>
2022-10-31 17:52:25 +0300

    admin

--- apitask/models.py
+++ apitask/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Task1(models.Model):
+    slackUsername=models.CharField(max_length=25)
+    backend=models.BooleanField(default=True)
+    age=models.IntegerField(default=0)
+    bio=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.slackUsername
+
+from django.contrib import admin
+admin.site.register(Task1)
