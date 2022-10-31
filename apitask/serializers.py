6e71002df5961478ec1af664cf7b693fa22c2191
Brian Nganga <briannganga70@gmail.com>
2022-10-31 22:07:38 +0300

    json

---
+++ apitask/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import Task1
+
+class Task1Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task1
+        fields = '__all__'
